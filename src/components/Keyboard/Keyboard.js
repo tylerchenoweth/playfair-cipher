f30a6627aa7a225a950893e6d518114537816289
Tyler Chenoweth <tchen358@gmail.com>
2025-06-06 20:19:25 +0000

    cleaned up the if else statements

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -8,7 +8,6 @@ import {useRef, useEffect} from 'react';
 
 function Keyboard({letterOne = undefined, letterTwo = undefined}) {
 
-
     const qwertyAlphabet = [
         ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
         ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
@@ -22,26 +21,18 @@ function Keyboard({letterOne = undefined, letterTwo = undefined}) {
         <div className="keyboard">
 
             {Array.from({length: qwertyAlphabet.length}).map((_, i) => 
-                // <p>{offsets[i]}</p>
-            
-
                 <div className="row">
                     {Array.from({length: qwertyAlphabet[i].length}).map((_, k) => 
                     
-                    qwertyAlphabet[i][k] === letterOne ? (
-                            <LetterSquare letter={qwertyAlphabet[i][k]} isPressed={true} />
-                    ) : qwertyAlphabet[i][k] === letterTwo ? (
+                    qwertyAlphabet[i][k] === letterOne || qwertyAlphabet[i][k] === letterTwo ? (
                         <LetterSquare letter={qwertyAlphabet[i][k]} isPressed={true} />
-                    ) : (
+                    ):(
                         <LetterSquare letter={qwertyAlphabet[i][k]} isPressed={false} />
                     ))}
                 </div>
-
             )}
-
         </div>
     );
-
 }
 
 export default Keyboard;
