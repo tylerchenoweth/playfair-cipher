f2f1445012e845d999330554c8f9954d87203d14
Tyler Chenoweth <tchen358@gmail.com>
2025-06-07 15:59:35 +0000

    changed the square array to a 2D array

--- src/components/PlayfairSquare/PlayfairSquare.js
+++ src/components/PlayfairSquare/PlayfairSquare.js
@@ -1,5 +1,6 @@
 
 import LetterSquare from '../LetterSquare/LetterSquare.js'
+import getCipherLetters from '../../utils/getCipherLetters.js'
 
 import './PlayfairSquare.css';
 
@@ -17,28 +18,72 @@ function PlayfairSquare({letterOne = undefined, letterTwo = undefined}) {
         }
     }).filter(Boolean);
 
-    const playfairSquare =[...keyword.split(''),...alphabetLeftovers]
+    // load cipher array
+    const playfairArr = [...keyword.split(''),...alphabetLeftovers]
 
+    const playfairSquare = [];
+    for(let i=0; i<5; i++) {
+        playfairSquare.push(playfairArr.slice(i * 5, i * 5 + 5));
+    }
     
+    let cipherLetterOne = undefined;
+    let cipherLetterTwo = undefined;
+
+    if(letterOne !== undefined && letterTwo !== undefined) {
+        const cipherLetters = getCipherLetters(playfairSquare, letterOne, letterTwo);
+        
+        console.log(letterOne, " -> ", cipherLetters[0]);
+        console.log(letterTwo, " -> ", cipherLetters[1]);
+
+        cipherLetterOne = cipherLetters[0];
+        cipherLetterTwo = cipherLetters[1];
+    }
+
+    console.log(cipherLetterOne);
+    console.log(cipherLetterTwo);
+
+    console.log(typeof(cipherLetterTwo));
+
     return (
         <div className="square">
-            {
-                Array.from({length: 6 }).map((_, i) => (
-                    <div className="row">
-                        {
-                            playfairSquare.slice(i*5,i*5+5).map((letter, index) => (
-                                letter === letterOne || letter === letterTwo ? (
-                                    <LetterSquare letter={letter} isPressed={true}/>
-                                ): letter === 'i' && (letterOne ==='j' || letterTwo ==='j') ? (
-                                    <LetterSquare letter={letter} isPressed={true}/>
-                                ):(
-                                    <LetterSquare letter={letter} isPressed={false}/>
-                                )
-                            ))
-                        }
-                    </div>
-                ))
-            }
+            
+            {/* {Array.from({length: 6 }).map((_, i) => (
+                <div className="row">
+                    {
+                        playfairSquare.slice(i*5,i*5+5).map((letter, index) => (
+                            letter === letterOne || letter === letterTwo ? (
+                                <LetterSquare letter={letter} isPressed={true}/>
+                            ): letter === 'i' && (letterOne ==='j' || letterTwo ==='j') ? (
+                                <LetterSquare letter={letter} isPressed={true}/>
+                            ):(
+                                <LetterSquare letter={letter} isPressed={false}/>
+                            )
+                        ))
+                    }
+                </div>
+            ))} */}
+
+            {Array.from({length: playfairSquare.length}).map((_,i) => (
+                <div className="row">
+                    {playfairSquare[i].map((letter, index) => (
+                        
+                        letter === letterOne || letter === letterTwo ? (
+                            <LetterSquare letter={letter} isPressed={true}/>
+                        ): letter === cipherLetterOne && cipherLetterOne !== undefined  ? (
+                            <LetterSquare letter={letter} isPressed={true}/>
+                        ): letter === cipherLetterTwo && cipherLetterTwo !== undefined ? (
+                            <LetterSquare letter={letter} isPressed={true}/>
+                        ): letter === 'i' && (letterOne ==='j' || letterTwo ==='j') ? (
+                            <LetterSquare letter={letter} isPressed={true}/>
+                        ): letter === 'i' && (cipherLetterOne ==='j' || cipherLetterTwo ==='j') ? (
+                            <LetterSquare letter={letter} isPressed={true}/>
+                        ):(
+                            <LetterSquare letter={letter} isPressed={false}/>
+                        )
+                    ))}
+                </div>
+            ))}
+            
         </div>
     );
 }
