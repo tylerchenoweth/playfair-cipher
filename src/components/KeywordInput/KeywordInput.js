ac0a5e1d93d2904a2f510d493adfb53492f9eb87
Tyler Chenoweth <tchen358@gmail.com>
2025-06-11 03:31:37 +0000

    added a component to get the keyword input

---
+++ src/components/KeywordInput/KeywordInput.js
@@ -0,0 +1,25 @@
+
+import isValidKeyword from "../../utils/isValidKeyword.js"
+
+import "./KeywordInput.css"
+
+import {useState} from 'react';
+
+function KeywordInput({value, onChange}) {
+
+    const handleInput = (e) => {
+        let keyword = e.target.value;
+
+        if(isValidKeyword(keyword) === true) onChange(e.target.value);
+    };
+      
+    return (
+        <div>
+            <input type="text" id="name" value={value} onChange={handleInput} />
+            
+        </div>
+    )
+
+}
+
+export default KeywordInput
