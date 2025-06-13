187f06160d40c6a4abb343b0230aebda94e4363d
Tyler Chenoweth <tchen358@gmail.com>
2025-06-13 19:05:06 +0000

    added a fontColor to change the color of the keyword letters in the ciphersquare

--- src/components/LetterSquare/LetterSquare.js
+++ src/components/LetterSquare/LetterSquare.js
@@ -2,22 +2,24 @@ import './LetterSquare.css'
 
 import {useState, useEffect} from 'react';
 
-function LetterSquare( {letter, isPressed=false, squareColor="blue"}) {
+function LetterSquare( {letter, isPressed=false, squareColor="blue", letterColor="white"}) {
 
     const [bgColor, setBgColor] = useState('transparent');
+    const [fontColor, setFontColor] = useState('white');
 
     // console.log("IS PRESSED", isPressed);
 
     useEffect(() => {
         
         setBgColor(squareColor);
+        setFontColor(letter);
             
         
     }, [squareColor]);
     
 
     return (
-        <div className="letterSquare" style={{background: bgColor}}> 
+        <div className="letterSquare" style={{background: bgColor, color: letterColor}}> 
             {letter}
             {/* <h1 className="letter" style={{background: bgColor}}>{letter}</h1> */}
         </div>
