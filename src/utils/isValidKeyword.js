5ccc1eb8b0127b6bbc60dabc42b3acc2181b0e3a
Tyler Chenoweth <tchen358@gmail.com>
2025-06-13 20:28:59 +0000

    there was some funk going on with accepting and display upper vs lowercase letters. It should only accept and show lowercase now

--- src/utils/isValidKeyword.js
+++ src/utils/isValidKeyword.js
@@ -1,7 +1,7 @@
 
 function isValidKeyword(keyword) {
-    if(keyword === "") return true;
-    if(!(/^[a-zA-Z]+$/.test(keyword))) return false;
+    // if(keyword === "") return true;
+    if( !(/^[a-zA-Z]+$/.test(keyword)) && keyword !== "") return false;
     if(keyword.length > 5) return false;
     if(keyword.includes('j')) return false;
 
