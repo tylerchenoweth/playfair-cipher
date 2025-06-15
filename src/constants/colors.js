bfd9818afb50989d6540be0b0effbc5070a7a0e3
Tyler Chenoweth <tchen358@gmail.com>
2025-06-15 01:02:05 +0000

    lightened the blue color by 20% and put the original blue and green in comments

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,16 +1,23 @@
 
 export const COLORS = {
-    myBlue: '#4F7CAC',
-    myGreen: '#6DCB76',
-    myOrange: '#FFA69E',
-    myBackground: '#0E1C36',
-  };
+  // myBlue: '#4F7CAC',
+  myBlue: '#6B96C2',
+  myGreen: '#6DCB76',
+  myOrange: '#FFA69E',
+  myBackground: '#0E1C36',
+};
+// OG blue
+// #4F7CAC
+// OG green
+// #8AEA92
+
+
+
   
-  
-  export const setCssVariables = (colors) => {
-    const root = document.documentElement;
-    Object.entries(colors).forEach(([key, value]) => {
-      root.style.setProperty(`--color-${key.toLowerCase()}`, value);
-    });
-  };
+export const setCssVariables = (colors) => {
+  const root = document.documentElement;
+  Object.entries(colors).forEach(([key, value]) => {
+    root.style.setProperty(`--color-${key.toLowerCase()}`, value);
+  });
+};
   
