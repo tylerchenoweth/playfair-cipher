69a6d6c541583992c693353467724a4747f7fae5
Tyler Chenoweth <tchen358@gmail.com>
2025-06-16 18:26:37 +0000

    added a section for the keyboard borders

--- src/utils/getBorder.js
+++ src/utils/getBorder.js
@@ -1,32 +1,57 @@
-export default function getBorder(isSquare, row, col) {
+export default function getBorder(isKeyboard, row, col) {
 
     let border;
 
-    const inW = "3px"; 
-    const outW = "6px"; 
+    const inW = "2px"; 
+    const outW = "4px"; 
 
-    if (row === 0) {
-        if (col === 0) {
-            border = `${outW} ${inW} ${inW} ${outW}`;
-        } else if (col === 4) {
-            border = `${outW} ${outW} ${inW} ${inW}`;
+
+    if(isKeyboard === true) {
+
+
+        if(row === 0) {
+            if(col === 0) {
+                border = `${outW} ${inW} ${outW} ${outW}`; 
+            } else if(col === 9) {
+                border = `${outW} ${outW} ${outW} ${inW}`;
+            } else {
+                border = `${outW} ${inW} ${outW} ${inW}`;
+            }
         } else {
-            border = `${outW} ${inW} ${inW} ${inW}`;
+            if(col === 0) {
+                border = `0 ${inW} ${outW} ${outW}`;
+            } else if(row === 1 && col === 8) {
+                border = `0 ${outW} ${outW} ${inW}`;
+            } else if(row === 2 && col === 6) {
+                border = `0 ${outW} ${outW} ${inW}`;
+            } else {
+                border = `0 ${inW} ${outW} ${inW}`;
+            }
         }
-    } else if (row === 4) {
-        if (col === 0) {
-            border = `${inW} ${inW} ${outW} ${outW}`;
-        } else if (col === 4) {
-            border = `${inW} ${outW} ${outW} ${inW}`;
-        } else {
-            border = `${inW} ${inW} ${outW} ${inW}`;
-        }
-    } else if (col === 0) {
-        border = `${inW} ${inW} ${inW} ${outW}`;
-    } else if (col === 4) {
-        border = `${inW} ${outW} ${inW} ${inW}`;
     } else {
-        border = `${inW} ${inW} ${inW} ${inW}`;
+        if (row === 0) {
+            if (col === 0) {
+                border = `${outW} ${inW} ${inW} ${outW}`;
+            } else if (col === 4) {
+                border = `${outW} ${outW} ${inW} ${inW}`;
+            } else {
+                border = `${outW} ${inW} ${inW} ${inW}`;
+            }
+        } else if (row === 4) {
+            if (col === 0) {
+                border = `${inW} ${inW} ${outW} ${outW}`;
+            } else if (col === 4) {
+                border = `${inW} ${outW} ${outW} ${inW}`;
+            } else {
+                border = `${inW} ${inW} ${outW} ${inW}`;
+            }
+        } else if (col === 0) {
+            border = `${inW} ${inW} ${inW} ${outW}`;
+        } else if (col === 4) {
+            border = `${inW} ${outW} ${inW} ${inW}`;
+        } else {
+            border = `${inW} ${inW} ${inW} ${inW}`;
+        }
     }
 
 
